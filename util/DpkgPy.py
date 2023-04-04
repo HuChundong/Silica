61133511197c6009dd873e28373ee3d7ab8e34b6
胡春东 <gycm520@gmail.com>
2023-04-04 20:01:55 +0800

    gpt error code

--- util/DpkgPy.py
+++ util/DpkgPy.py
@@ -26,28 +26,28 @@ class DpkgPy:
             root_ar = arpy.Archive(input_path)
             root_ar.read_all_headers()
             
-            control_bin_ext = None
+            data_bin_ext = None
 
             for ext in ['.gz', '.lzma', '.xz', '.zst']:
-                if b'control.tar' + ext.encode() in root_ar.archived_files:
-                    control_bin_ext = ext
+                if b'data.tar' + ext.encode() in root_ar.archived_files:
+                    data_bin_ext = ext
                     break
 
-            if control_bin_ext is None:
-                raise ValueError("Unsupported control_bin format")
+            if data_bin_ext is None:
+                raise ValueError("Unsupported data_bin format")
 
-            control_bin = root_ar.archived_files[b'control.tar' + control_bin_ext.encode()]
+            data_bin = root_ar.archived_files[b'data.tar' + data_bin_ext.encode()]
 
-            if control_bin_ext == '.gz':
-                control_tar = tarfile.open(fileobj=control_bin)
-            elif control_bin_ext == '.lzma' or control_bin_ext == '.xz':
-                control_data = lzma.decompress(control_bin.read())
-                control_tar = tarfile.open(fileobj=io.BytesIO(control_data))
-            elif control_bin_ext == '.zst':
-                control_data = pyzstd.decompress(control_bin.read())
-                control_tar = tarfile.open(fileobj=io.BytesIO(control_data))
+            if data_bin_ext == '.gz':
+                data_tar = tarfile.open(fileobj=data_bin)
+            elif data_bin_ext == '.lzma' or data_bin_ext == '.xz':
+                data_data = lzma.decompress(data_bin.read())
+                data_tar = tarfile.open(fileobj=io.BytesIO(data_data))
+            elif data_bin_ext == '.zst':
+                data_data = pyzstd.decompress(data_bin.read())
+                data_tar = tarfile.open(fileobj=io.BytesIO(data_data))
 
-            control_tar.extractall(output_path)
+            data_tar.extractall(output_path)
             print(output_path)
             return True
         except Exception:
